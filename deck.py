607149f61ce0cb8085897468ad38d1d63753383e
Hasan Rashad <fitthasan@gmail.com>
2023-05-10 06:01:07 +0300

    Create deck class

---
+++ deck.py
@@ -0,0 +1,20 @@
+import itertools
+
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+
+    def __len__(self):
+        return len(self.cards)
+
+    def add_cards(self):
+        ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+        suits = ['spades', 'clubs', 'hearts', 'diamonds']
+        black_jack_deck = itertools.product(ranks, suits)
+        [self.cards.append(card) for card in black_jack_deck]
+
+
+deck = Deck()
+deck.add_cards()
+print(len(deck))
